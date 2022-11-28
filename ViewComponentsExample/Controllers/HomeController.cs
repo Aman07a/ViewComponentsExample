944991d68f19ead09490c06f6ce150682dec3c7e
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-28 17:19:49 +0100

    ViewComponentResult

--- ViewComponentsExample/Controllers/HomeController.cs
+++ ViewComponentsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.Controllers
 {
@@ -14,6 +15,32 @@ namespace ViewComponentsExample.Controllers
 		public IActionResult About()
 		{
 			return View();
+		}
+
+		[Route("friends-list")]
+		public IActionResult LoadFriendsList()
+		{
+			PersonGridModel personGridModel = new PersonGridModel()
+			{
+				GridTitle = "Friends",
+				Persons = new List<Person>()
+				{
+					new Person() {
+						PersonName = "Mia",
+						JobTitle = "Developer"
+					},
+					new Person() {
+						PersonName = "Emma",
+						JobTitle = "UI Designer"
+					},
+					new Person() {
+						PersonName = "Avva",
+						JobTitle = "QA"
+					}
+				}
+			};
+
+			return ViewComponent("Grid", new { grid = personGridModel });
 		}
 	}
 }

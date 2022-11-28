954f491b8b28e4bac50babdf91503bae34523808
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-28 15:26:24 +0100

    Creating View Components - Part 1

---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+	public class GridViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			// Invoked a partial view: Views/Shared/Components/Grid/Default.cshtml
+			return View("Sample");
+		}
+	}
+}
